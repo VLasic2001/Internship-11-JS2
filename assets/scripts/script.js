a6278c4a1073a215ae5f6cf00256bb681e6f7599
VLasic2001 <vedro.lasic@gmail.com>
2019-03-17 12:17:37 +0100

    Finished second task

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -67,3 +67,58 @@ sortList.innerHTML = sortArray.map(item => `<li class="dropdown__list__item">${i
 productTypeList.innerHTML = productTypeArray.map(item => `<li class="dropdown__list__item">${item.type} <span class="list__item__count"> (${item.count})</span></li>`).join('');
 sleeveLengthList.innerHTML = sleeveLengthArray.map(item => `<li class="dropdown__list__item">${item.type} <span class="list__item__count"> (${item.count})</span></li>`).join('');
 necklineList.innerHTML = necklineArray.map(item => `<li class="dropdown__list__item">${item.type} <span class="list__item__count"> (${item.count})</span></li>`).join('');
+
+let itemsArray = [
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: "Wrangler small logo crew neck t-shirt in white",
+    price: "£16.63"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: "Wrangler logo chest stripe rugby polo in blue/white",
+    price: "£50.63"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: "Wrangler kobel retro large logo ringer t-shirt in white",
+    price: "£20.97"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: "Lyle & Scott polo in burgundy",
+    price: "£44.12"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: '<span class="item__paragraph__brand">COLLUSION</span> Unisex long sleeve t-shirt with graphic print in neon green',
+    price: "£12.29"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: '<span class="item__paragraph__brand">ASOS DESIGN</span> Disney oversized t-shirt with rainbow',
+    price: "£22.42"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: '<span class="item__paragraph__brand">COLLUSION</span> Unisex oversized t-shirt with back print',
+    price: "£10.12"},
+
+    {src: "assets/images/offer.jpg",
+    description: "TALL",
+    paragraph: '<span class="item__paragraph__brand">ASOS DESIGN</span> Mickey reloxed t-shirt with retro print',
+    price: "£18.08"},
+];
+
+let items = document.getElementById("items");
+
+items.innerHTML = itemsArray.map(item =>`
+<div class="offers__item">
+<img class="item__img" src="${item.src}" alt="Offer" />
+<img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
+<img class="item__heart-full" src="./assets/images/heart-full.png" alt="Hearth Full">
+<p class="item__img-description">${item.description}</p>
+<p class="item__paragraph">${item.paragraph}</p>
+<span class="item__price">${item.price}</span>
+</div>
+`);
